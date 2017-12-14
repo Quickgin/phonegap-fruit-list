ff1d8f7f479e2154fc878ad67931d058bee26335
Quickgin <wanl2_15@uni.worc.ac.uk>
2017-12-14 14:11:58 +0000

    first

---
+++ exercise2/exercise2/js/index.js
@@ -0,0 +1,23 @@
+$(document).on("pagecreate","#pageone", onPageCreated);
+
+//some example data
+fruit = [
+  { name: 'Banana', 	colour: 'Yellow', cost: '£2', country:'UK'		},
+  { name: 'Orange',  colour: 'Orange', cost: '£2', country:'UK' 	},
+  { name: 'Lime',	colour: 'Green', cost: '£1', country:'UK'		}
+];
+
+
+function onPageCreated() {
+	var ractive = new Ractive({
+		  // The `el` option can be a node, an ID, or a CSS selector.
+		  el: '#content',
+	
+		  // We could pass in a string, but for the sake of convenience
+		  // we're passing the ID of the <script> tag above.
+	  template: '#template',
+	
+		  // Here, we're passing in some initial data
+		  data: { fruitlist: fruit }
+		});
+}
